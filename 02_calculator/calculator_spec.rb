0246ff598cfc3c36c103cf2c7247950ffb086b33
munan0317 <munan0317@gmail.com>
2016-10-21 00:17:11 -0400

    complete test for multiple power and factorial

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -81,11 +81,12 @@ describe "multiply" do
 
   it "multiplies two numbers" do
     multiply([7,9]).should == 63
-end
+  end
 
   it "multiplies several numbers" do
       multiply([1,3,5,7,9]).should == 945
-end
+    end
+  end
 
 describe "#power" do
   it "raises one number to the power of another number" do
@@ -95,9 +96,19 @@ end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+  it "computes the factorial of 0" do
+    factorial(0).should == 1
+  end
+  it "computes the factorial of 1" do
+    factorial(1).should == 1
+  end
+  it "computes the factorial of 2" do
+    factorial(2).should == 2
+  end
+  it "computes the factorial of 5" do
+    factorial(5).should == 120
+  end
+  it "computes the factorial of 10" do
+    factorial(10).should == 3_628_800
+  end
 end
